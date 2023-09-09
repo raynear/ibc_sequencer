c4d47869a9c1695669d7ec1bfc355b62f7df571e
gylman <gylmanbm@gmail.com>
2023-09-10 01:03:17 +0900

    implement time-lock puzzle and its solution

---
+++ tlp/tlp.js
@@ -0,0 +1,33 @@
+const crypto = require("crypto");
+
+// This is the seed which we will give to the sequencer
+const seed = "Hello World";
+const timeInSeconds = 10;
+
+// Create time-lock puzzle with the seed and desired time. This function will hash the seed until the time is up. The final hash will be our decryption key.
+function createPuzzle(seed, timeInSeconds) {
+  let key = seed;
+  const startTime = Date.now();
+  const endTime = startTime + timeInSeconds * 1000; // Convert seconds to milliseconds
+  let iters = 0;
+  while (Date.now() < endTime) {
+    // Repetitively hash and count iterations
+    key = crypto.createHash("sha256").update(key).digest("hex");
+    iters++;
+  }
+  return { key, iters };
+}
+
+function solvePuzzle(seed, iters) {
+  let key = seed;
+  for (let i = 0; i < iters; i++) {
+    key = crypto.createHash("sha256").update(key).digest("hex");
+  }
+  return key;
+}
+
+const { key, iters } = createPuzzle(seed, timeInSeconds);
+
+// Test
+
+console.log(key === solvePuzzle(seed, iters));
